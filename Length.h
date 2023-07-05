8209e1639769a7c4624f1fbfd93f3f008f0ef635
Isaac <ik.appiagyei@gmail.com>
2023-07-05 10:28:19 +0200

    added the comparison operator overload as well

--- Length.h
+++ Length.h
@@ -4,10 +4,20 @@
 class Length
 {
 public:
+	// Constructor
 	explicit Length(int value);
+
+	// Equality Operators
 	bool operator ==(const Length& other) const;
+
 	bool operator ==(int other) const;
 	bool operator !=(int other) const;
+
+	//Comaparison Operators
+	bool operator <(const Length& other) const;
+	bool operator <=(const Length& other) const;
+	bool operator >(const Length& other) const;
+	bool operator >=(const Length& other) const;
 
 
 private:
